495168d80be309e391ee009373f59a6ec540fb91
3BoOoD97 <abodn70@hotmail.com>
2023-06-28 18:43:19 +0200

    Write a Pseudocode for task 1

--- Console_App/Program.cs
+++ Console_App/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Console_App 
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+
+            /* Pseudocode
+                   ** PART 1 **
+                   * Send arguments to the Main method while executing the code.
+                   * Catch and handle errors if any.
+                     1- Check if the number of arguments is not equal to 2 then print an error message and exit the program.
+                   * Create a new instance from ExtensionMethods and call add method
+                   * Display the result
+                   * End of PART 1
+                    ----------------
+                    
+                   
+                    */
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
